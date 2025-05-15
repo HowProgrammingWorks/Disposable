da3325cf01f8ba897c3c7707a767941036daeb8f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-05-15 04:06:16 +0300

    Show difference between sync and async dispose

---
+++ JavaScript/5-async.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const fs = require('node:fs/promises');
+const timers = require('timers/promises');
+
+class Logger {
+  #file = null;
+
+  constructor(path) {
+    return this.#init(path);
+  }
+
+  async #init(path) {
+    this.#file = await fs.open(path, 'a');
+    return this;
+  }
+
+  async log(message) {
+    const timestamp = new Date().toISOString();
+    const msg = `[${timestamp}] ${message}`;
+    console.log(msg);
+    await this.#file?.write(msg + '\n');
+  }
+
+  async [Symbol.asyncDispose]() {
+    console.log('Going to close file (dispose)');
+    await this.#file.close();
+    console.log('File closed (dispose)');
+    await timers.setTimeout(1000);
+    console.log('After timeout (dispose)');
+  }
+}
+
+const main = async () => {
+  await using logger = await new Logger('./output.log');
+  await logger.log('Open');
+  await logger.log('Do something');
+};
+
+main().then(() => {
+  console.log('After dispose');
+});
